28ff0c9f90f6c2e14f491440603f89773a66ac38
Fahmi Sboui <fahmisboui1@gmail.com>
2023-05-20 02:23:57 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html","./dist/*.js"],
+  content: ["./*.html","./*.js"],
   theme: {
     extend: {
       screens: {
